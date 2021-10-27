d499d2f8373331d0c28978d2d4a2247c153e3072
ErlingHW <erlingstaff@gmail.com>
2021-10-27 21:33:20 +0200

    nav-bar almost done

--- Resources/scripts/index.js
+++ Resources/scripts/index.js
@@ -4,7 +4,7 @@ var navbar = document.getElementById("navBar");
 var sticky = navbar.offsetTop; // Kopiert fra w3schools
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= sticky) {   
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
